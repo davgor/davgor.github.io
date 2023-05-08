f2cd3cd86e4242af26c65908a272401c7a5f1227
davgor <davgor4@gmail.com>
2023-05-07 21:19:00 -0400

    spell check + clearBody

--- src/scripts.js
+++ src/scripts.js
@@ -18,32 +18,16 @@ function aboutMePage() {
                     <div class="card-body">
                         <h5 class="card-title">David Gorden - 7+ years experience</h5>
                         <p class="card-text">
-                            My name is David Gorden, I have been an engineer for over 7 years now, during this time I have consistently 
-                            rose to the challeneges presented, and often natually find myself adopting a leadership position within 
-                            each employer. 
+                            My name is David Gorden, I have been an engineer for over 7 years now, during this time I have consistently rose to the challenges presented, and often naturally find myself adopting a leadership position within each employer. 
                         </p>
                         <p class="card-text">
-                            A bit of a background as too who I am and how I got here, I went into army when I was 20, I didn't know 
-                            what I wanted to do, while I was in basic training, I was thinking truely on what I wanted to do 
-                            post-military, and I realized the most fun I had doing work, was when I was modding oblivion while 
-                            I was a teen. This motivation is what drove me into the computer science track. After doing some more 
-                            research as to my career options, I realized programming, and development was the specific section 
-                            of game development I was interested in.
+                            A bit of a background as too who I am and how I got here, I went into army when I was 20, I didn't know what I wanted to do, while I was in basic training, I was thinking truly on what I wanted to do post-military, and I realized the most fun I had doing work, was when I was modding oblivion while I was a teen. This motivation is what drove me into the computer science track. After doing some more research as to my career options, I realized programming, and development was the specific section of game development I was interested in.
                         </p>
                         <p class="card-text">
-                            After finishing my associates degree, I got a job as a developer for a mid-sized insurance company in the 
-                            midwest, After developing for a time, I wanted to reduce the time I was spending completing manual tests 
-                            on the webpages I was working on, after talking with the QA manager, they created, and brought me into 
-                            a pilot program where I learned how to write automated tests, and implement them for our teams. Thats 
-                            how I found myself in Quality Engineering, and I love every minute of it.
+                            After finishing my associates degree, I got a job as a developer for a mid-sized insurance company in the Midwest, after developing for a time, I wanted to reduce the time I was spending completing manual tests on the webpages I was working on, after talking with the QA manager, they created, and brought me into a pilot program where I learned how to write automated tests, and implement them for our teams. That's how I found myself in Quality Engineering, and I love every minute of it.
                         </p>
                         <p class="card-text">
-                            Outside of my professional story, I am a huge gamer, I play just about everything, on every game system, 
-                            my most frequent game is currently Elder Scrolls Online, though I think Diable IV is going to take over here 
-                            soon. I also spend my quiet time painting miniatures for DND, and Warhammer. It is definitely the dorkiest 
-                            thing about me, but painting is such a relaxing activity, and an excellent way to reset your mind. I also have 3 
-                            dogs, Bilbo, Dorian, and Lestat, at the start of the pandemic we moved off to Shallotte, North Carolina to live 
-                            close to the beach, one of those beaches is where that background photo is taken.
+                            Outside of my professional story, I am a huge gamer, I play just about everything, on every game system, my most frequent game is currently Elder Scrolls Online, though I think Diablo IV is going to take over here soon. I also spend my quiet time painting miniatures for DND, and Warhammer. It is definitely the dorkiest thing about me, but painting is such a relaxing activity, and an excellent way to reset your mind. I also have 3 dogs, Bilbo, Dorian, and Lestat, at the start of the pandemic we moved off to Shallotte, North Carolina to live close to the beach, one of those beaches is where that background photo is taken.
                         </p>
                     </div>
                 </div>
