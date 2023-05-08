2b3e9bfa2c63ffa5c5f4cbc178a367875e11ff5a
davgor <davgor4@gmail.com>
2023-05-08 18:35:26 -0400

    experience page
    Adding Lyft and Atreo to my experience page

---
+++ src/pages/aboutMePage.js
@@ -0,0 +1,29 @@
+function aboutMePage() {
+    clearBody();
+    addToBody(`
+        <div class="card mb-3 bg-transparent border-0" style="max-width: 100%;">
+            <div class="row g-0 cardmargins">
+                <div class="col-md-4">
+                    <img src="./assets/fun_personal_photo.jpg" class="img-fluid rounded-start" alt="Personal Photo">
+                </div>
+                <div class="col-md-8 cardbackdrop">
+                    <div class="card-body">
+                        <h5 class="card-title">David Gorden - 7+ years experience</h5>
+                        <p class="card-text">
+                            My name is David Gorden, I have been an engineer for over 7 years now, during this time I have consistently rose to the challenges presented, and often naturally find myself adopting a leadership position within each employer. 
+                        </p>
+                        <p class="card-text">
+                            A bit of a background as too who I am and how I got here, I went into army when I was 20, I didn't know what I wanted to do, while I was in basic training, I was thinking truly on what I wanted to do post-military, and I realized the most fun I had doing work, was when I was modding oblivion while I was a teen. This motivation is what drove me into the computer science track. After doing some more research as to my career options, I realized programming, and development was the specific section of game development I was interested in.
+                        </p>
+                        <p class="card-text">
+                            After finishing my associates degree, I got a job as a developer for a mid-sized insurance company in the Midwest, after developing for a time, I wanted to reduce the time I was spending completing manual tests on the webpages I was working on, after talking with the QA manager, they created, and brought me into a pilot program where I learned how to write automated tests, and implement them for our teams. That's how I found myself in Quality Engineering, and I love every minute of it.
+                        </p>
+                        <p class="card-text">
+                            Outside of my professional story, I am a huge gamer, I play just about everything, on every game system, my most frequent game is currently Elder Scrolls Online, though I think Diablo IV is going to take over here soon. I also spend my quiet time painting miniatures for DND, and Warhammer. It is definitely the dorkiest thing about me, but painting is such a relaxing activity, and an excellent way to reset your mind. I also have 3 dogs, Bilbo, Dorian, and Lestat, at the start of the pandemic we moved off to Shallotte, North Carolina to live close to the beach, one of those beaches is where that background photo is taken.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `);
+}
---
+++ src/pages/experiencePage.js
@@ -0,0 +1,134 @@
+function experiencePage() {
+    clearBody();
+    lyftCard();
+    atreoCard();
+}
+
+function lyftCard() {
+    addToBody(`
+        <div class="card mb-3 bg-transparent border-0" style="max-width: 100%;">
+            <div class="row g-0 cardmargins">
+                <div class="col-md-2 cardbackdrop">
+                    <a href="https://www.lyft.com/">
+                        <img src="./assets/lyft-logo-transparent.png" class="img-fluid rounded-start" alt="Lyft Logo">
+                    </a>
+                </div>
+                <div class="col-md-10 cardbackdrop">
+                    <div class="card-body">
+                        <h5 class="card-title">Lyft - Software Engineer in Test - 09/15/2022 - 04/28/2023</h5>
+                        <p class="card-text">
+                            Lyft was my first breakthrough into a big tech organization, here I was hired to be a Software developer in test, with the core tech stack being Kotlin/Espresso for android testing, and Swift/XCUITest for mobile testing. As SDET's for Lyft we were not only charged with building and expanding upon the mobile testing frameworks, but also leading the charge for quality assurance on an individual project level. We would be assigned to a specific project, and then we would create a comprehensive test plan to be manually tested through the project's development.
+                        </p>
+                        <p class="card-text">
+                            This iterative approach is known as <a href="https://www.testim.io/blog/shift-left-testing-guide/">Shift Left Testing </a> allowed us create an extensive test plan, and keep the projects quality high through the whole development cycle. Once a project was completed, we would assess the project for regression tests, and add them to our weekly release testing cycle. From there we would begin to automate our tests.
+                        </p>
+                        <p class="card-text">
+                            Once we assess the automatable test scenarioes we would divide the test cases into several different test applications.
+                        </p>
+                        <ul>
+                            <li>
+                                UI Testing (Espresso/XCUITest) - We leveraged these tools for UI tests that required navigation to other screens.
+                            </li>
+                            <li>
+                                <a href="https://eng.lyft.com/shift-left-ios-testing-with-focus-flows-c9e050bd2095">Focus Flow Testing</a> - This was used for testing 1 single screen quickly with a drastically reduced build time
+                            </li>
+                            <li>
+                                Acceptance Testing - was an in house framework for API flow testing, allowing us to test more expansive test flows that could involve multiple apps, or users
+                            </li>
+                        </ul>
+                        <p class="card-text">
+                            Once we had our tests automated, we would set up our jobs in Buildkite to automatically run (normally daily), and give the developers the ability to run the suites through their PR's on github directly.
+                        </p>
+                        <h6><b>Achievements</b></h6>
+                        <ul>
+                            <li>
+                                Boosted manual test efficiency by 20x by formulating a testing process for multi-phone test cases, allowing any employee to test a complex feature individually.
+                            </li>
+                            <li>
+                                Reduced manual efforts required to test driver bonuses from 2 hours to 2 minutes by collaborating with SDETs to develop a new process, allowing for instant bonus creation via <b>Python</b>
+                            </li>
+                            <li>
+                                Lowered manual testing efforts through automation from 16 hours to 2 hours by prioritizing high-value tests for automation, and optimizing testing coverage.
+                            </li>
+                            <li>
+                                Minimized regression testing time by 75% from 16 hours to 4 hours through strategic automation development, improving testing efficiency and effectiveness.
+                            </li>
+                            <li>
+                                Implemented a new testing tool and mentored execution of use cases, resulting in a 25% reduction in test execution time and an overall enhancement of the testing suites.
+                            </li>
+                            <li>
+                                Created and pushed new automation best practices to follow to reduce weekly maintaince load on test engineers, while expanding test suite to focus on the highest value items.                            </li>
+                        <ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `);
+}
+
+function atreoCard() {
+    addToBody(`
+        <div class="card mb-3 bg-transparent border-0" style="max-width: 100%;">
+            <div class="row g-0 cardmargins">
+                <div class="col-md-2 cardbackdrop">
+                    <a href="https://www.atreo.io/">
+                        <img src="./assets/Atreo_logo_transparent.png" class="img-fluid rounded-start" alt="Atreo Logo">
+                    </a>
+                </div>
+                <div class="col-md-10 cardbackdrop">
+                    <div class="card-body">
+                        <h5 class="card-title">Atreo.io - Software Automation Engineer - 07/23/2021 - 09/15/2022</h5>
+                        <p class="card-text">
+                            I was hired around the initial development of atreo's software, initially I helped expand out a <b>.net</b> 
+                            API that existed to help translate test results from unit, integrations, and UI tests. Then convert them 
+                            into <b>SpiraTeam</b> test cases and executions. After creating that initial support infastructure I 
+                            began to create <b>Playwright</b> UI tests, where I would develop extensive, and versitile new test suites for 
+                            each feature they developed.
+                        </p>
+                        <p class="card-text">
+                            Once the tests were created we needed a reliable way to execute the tests directly on the developers PR's 
+                            While still being efficent enough to not block further development, which is quite a challenge when you 
+                            are executing 2500 UI test cases. We did this by leveraging a feature in Github actions called matrix, and a 
+                            playwright feature called sharding. With this we were able to divide the test suite into small sections divided 
+                            by the number of shards, each of the browsers that Playwright supports, and pass them into the github matrix. This
+                            would then create hundreds of small jobs, only downloading and installing the right browsers, and completing the 
+                            entire test sutie in around 5 minutes.
+                        </p>
+                        <p class="card-text">
+                            Coupling this with a listener on <b>AWS</b> that would track incoming requests for a service on Github actions, then 
+                            spin up a new server that contained a pre-built image for our QA build. Then be assigned the jobs outside of the matrix. 
+                            Once they were no longer needed they would be collapsed and removed. By doing this we were able to drastically reduce 
+                            execution time, without sacraficing the test suites value.
+                        </p>
+                        <p class="card-text">
+                        Once the infrastructure to handle massive test suites was completed, we began to expand the automation engineer team, adding 2 
+                        additional automation engineers, who I then mentored on automation practices, <b>Typescript</b> development, 
+                        <b>Data Driven Testing</b>, and then managed their resourcing to different products within Atreo.io. I also helped design and 
+                        implement the hiring test to assess the skill levels of new incoming automation engineers, this allowed us to gauge an 
+                        individual's speed in learning a new automation framework, and got them more familiar with the company by having them crawl 
+                        on our corporate website. 
+                        </p>
+                        <h6><b>Achievements</b></h6>
+                        <ul>
+                            <li>
+                                Reduced testing time, from 2 weeks to 2 hours, in a highly standardized industry, enabling organizations to meet tight project timelines.
+                            </li>
+                            <li>
+                                Decreased AWS expenses by 70% by improving test efficiency and optimizing backend testing leading to significant cost savings for the organization.
+                            </li>
+                            <li>
+                                Minimized testing timeline from 2 weeks to 2 hours by revolutionizing RTSM industry's standard testing process through creating an automated test approach involving data driving test cases.
+                            </li>
+                            <li>
+                                Improved testing process proficiency by creating a comprehensive test suite consisting of over 5000 test cases within a year while designing test cases to be applied for each client, developing test suites of over 2500 test cases in minutes.
+                            </li>
+                            <li>
+                                Lead a small team of Automation Engineers, and up-skilled manual quality assurance engineers to quality automation engineers.
+                            </li>
+                        <ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `);
+}
